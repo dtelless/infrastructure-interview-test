040c1f7696b4a012ab6debcb4e82a9bf1a80dcfe
Daniel Sousa <dtelless@gmail.com>
2026-01-15 12:17:26 -0300

    refactor(infra): switch to remote helm chart and update image repository
    - Update app image repository to 'dtelless/infrastructure-interview-test' in Helm values.
    - Configure Terraform to pull the 'typeorm-app' chart from GHCR OCI registry instead of a local path.

--- infraestructure/terraform/app.tf
+++ infraestructure/terraform/app.tf
@@ -1,17 +1,13 @@
 resource "helm_release" "typeorm_app" {
-  name = "typeorm-app"
-  # Use Local Configuration for development/testing
-  chart = "${path.module}/../charts/typeorm-app"
-
-  # To use GitHub Container Registry (after publishing the chart):
-  # repository = "oci://ghcr.io/YOUR_USERNAME/charts"
-  # chart      = "typeorm-app"
+  name       = "typeorm-app"
+  repository = "oci://ghcr.io/dtelless/charts"
+  chart      = "typeorm-app"
 
   version = "0.1.0"
 
-  values = [
-    file("${path.module}/../charts/typeorm-app/values.yaml")
-  ]
+  # values = [
+  #   file("${path.module}/../charts/typeorm-app/values.yaml")
+  # ]
 
   depends_on = [kind_cluster.default, helm_release.mariadb]
 }
