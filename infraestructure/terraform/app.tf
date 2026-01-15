b7d9d6039f977cd3db69219e7074ef9940b273d3
Daniel Sousa <dtelless@gmail.com>
2026-01-15 14:21:42 -0300

    refactor(infra): update helm chart repository to Docker Hub
    Update the 'typeorm-app' Helm release to use the Docker Hub OCI registry instead of GitHub Container Registry.

--- infraestructure/terraform/app.tf
+++ infraestructure/terraform/app.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "typeorm_app" {
   name       = "typeorm-app"
-  repository = "oci://ghcr.io/dtelless/charts"
+  repository = "oci://registry-1.docker.io/dtelless"
   chart      = "typeorm-app"
+  version    = "0.1.0"
 
-  version = "0.1.0"
 
   # values = [
   #   file("${path.module}/../charts/typeorm-app/values.yaml")
